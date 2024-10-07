1ff00cadbdce2196b25624e870d5eb484e2726d0
Sanny Kumar <154778296+SANNY08@users.noreply.github.com>
2024-10-08 00:13:58 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int  main()
+{
+    int n,no;
+    cout<<"Enter two number:";
+    cin>>n>>no;
+    cout<<"Addition is :"<<n+no;
+    getch();
+}
